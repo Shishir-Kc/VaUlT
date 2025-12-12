be3b4c1976b77341f0280dfe321f4f06fe0a0dc6
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-12-12 18:26:55 +0545

    made a proper user login and user creation endpoint

--- backend_end/server/main.py
+++ backend_end/server/main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI,Depends,HTTPException,status
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
-import jwt
-from auth.user_auth import fake_users_db
 from fastapi.security import OAuth2PasswordRequestForm
 from auth.auth_model import Token,User
 from auth.user_auth import authenticate_user,get_current_active_user
 from datetime import timedelta
 from auth.auth_config import ACCESS_TOKEN_EXPIRE_MINUTES
 from auth.jwt.generate_jwt import create_acess_token
+from data_base.data_base_config import engine
+from sqlmodel import SQLModel,Session,select
+from data_base.data_base_ import UserCreate,UseRead
+from data_base.data_base_ import User as U 
+from auth.pass_hash import hash_password
+from data_base.data_base_config import get_session
 
 app = FastAPI()
 
@@ -43,6 +47,17 @@ user_pass = {
 }
 
 
+def init_database():
+   SQLModel.metadata.create_all(engine)
+
+
+session_dep = Annotated[Session,Depends(get_session)]
+
+@app.on_event('startup')
+def geenrate_table():
+   init_database()
+
+
 @app.get('/user/passwords/')
 def user_dashboard():
     return user_pass
@@ -50,10 +65,10 @@ def user_dashboard():
 
 @app.post('/user/login/')
 def user_login(
-    form:Annotated[OAuth2PasswordRequestForm,Depends()]):
-    print(form.username)
-    user = authenticate_user(fake_users_db,form.username,form.password)
-    # print(user.username)
+    form:Annotated[OAuth2PasswordRequestForm,Depends()],session:session_dep):
+
+    user = authenticate_user(session,form.username,form.password)
+
     if not user: 
      raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -75,3 +90,23 @@ def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
+
+@app.post("/user/create/")
+def create_users(user:UserCreate,session:session_dep)->UseRead:
+   hashed_pass = str(hash_password(user.password))
+   user_db = U.model_validate(
+      user,
+      update={'password':hashed_pass}
+   )
+   session.add(user_db)
+   session.commit()
+   session.refresh(user_db)
+
+   return user_db
+
+
+@app.get('/users/all/',response_model=list[UseRead])
+def get_all_users(session:session_dep,current_user:Annotated[User,Depends(get_current_active_user)]):
+    statement = select(U)
+    users = session.exec(statement).all()
+    return users
