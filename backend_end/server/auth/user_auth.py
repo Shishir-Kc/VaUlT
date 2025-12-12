56aa26afce0dfbbb0f1ecd361adc46606f5fc740
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-12-12 18:25:08 +0545

    feat: removed fake database

--- backend_end/server/auth/user_auth.py
+++ backend_end/server/auth/user_auth.py
@@ -6,19 +6,12 @@ from .pass_hash import oauth2_scheme,verify_password
 import jwt
 from .auth_config import SECRET_KEY,ALGORITHM
 from .auth_model import TokenData,User
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
-        "disabled": False,
-    }
-}
+from data_base.data_base_ import User
+from sqlmodel import select,Session
+from data_base.data_base_config import get_session
 
 
-def get_current_user(token: Annotated[str,Depends(oauth2_scheme)]):
+def get_current_user(session: Annotated[Session, Depends(get_session)],token: Annotated[str,Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="user not found",
@@ -33,26 +26,25 @@ def get_current_user(token: Annotated[str,Depends(oauth2_scheme)]):
     except InvalidTokenError:
         raise credentials_exception
     
-    user = get_user(fake_users_db,username=token_data.username)
+    user = get_user(session,username=token_data.username)
     if user is None:
         raise credentials_exception
     print(f"user hehe{user}")
     return user
 
 
-def get_user(db,username:str):
-    if username in db:
-        user_dict = db[username]
-       
-        return UserInDB(**user_dict) 
+def get_user(session: Session, username: str): 
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement).first()
+    return user
 
-def authenticate_user(fake_databae,username:str,password:str):
-    user = get_user(fake_databae,username)
+def authenticate_user(session:Session,username:str,password:str):
+    user = get_user(session,username)
     if not user:
         print('user does not exists !')
         return False
 
-    if not verify_password(password,user.hashed_password):
+    if not verify_password(password,user.password):
         print("invalid pass")
         return False
     
@@ -63,6 +55,5 @@ def authenticate_user(fake_databae,username:str,password:str):
 def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+
     return current_user
