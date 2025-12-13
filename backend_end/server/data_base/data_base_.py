803c18803f43343a54c69c447a93b81a1491fc6d
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-12-13 19:42:51 +0545

    feat: added one to many relationship and added userpasswords table

--- backend_end/server/data_base/data_base_.py
+++ backend_end/server/data_base/data_base_.py
@@ -1,5 +1,23 @@
-from sqlmodel import SQLModel,Field
+from sqlmodel import SQLModel,Field,Relationship
 import uuid
+from typing import List,Optional
+
+
+class UserPasswordCreate(SQLModel):
+    user_gmail :str
+    user_password :str
+    application:str
+    application_icon:Optional[str]=None
+
+class UserPasswordRead(UserPasswordCreate):
+    id:uuid.UUID
+   
+
+class UserPasswords(UserPasswordCreate,table=True):
+    id:uuid.UUID = Field(default_factory=uuid.uuid4,primary_key=True)
+    user_id : uuid.UUID = Field(foreign_key='user.id')
+    user:"User" = Relationship(back_populates='user_passwords')
+
 
 class UserBase(SQLModel):
     email:str
@@ -8,10 +26,13 @@ class UserBase(SQLModel):
 class User(UserBase,table=True):
     id:uuid.UUID=Field(default_factory=uuid.uuid4,primary_key=True)
     password:str
+    user_passwords:List["UserPasswords"] = Relationship(back_populates='user')
 
 class UserCreate(UserBase):
     password:str
 
-class UseRead(UserBase):
+class UserRead(UserBase):
     id:uuid.UUID
     
+
+
